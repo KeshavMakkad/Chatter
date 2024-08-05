7e21bc72434e2446ac4de3408fac44caaae38487
KeshavMakkad <keshav.23bcs10002@ms.sst.scaler.com>
2024-08-05 22:36:00 +0530

    video chatting works

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,11 +1,169 @@
-import { useEffect } from "react";
+import { createRef, LegacyRef, useEffect, useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { Socket, io } from "socket.io-client";
 
-export const Room = () => {
+const URL = "http://localhost:3000";
+
+export const Room = ({
+    name,
+    localAudioTrack,
+    localVideoTrack,
+}: {
+    name: string;
+    localAudioTrack: MediaStreamTrack | null;
+    localVideoTrack: MediaStreamTrack | null;
+}) => {
     const [searchParams, setSearchParams] = useSearchParams();
-    const name = searchParams.get("name");
+    const [lobby, setLobby] = useState(true);
+    const [socket, setSocket] = useState<null | Socket>(null);
+    const [sendingPc, setSendingPc] = useState<null | RTCPeerConnection>(null);
+    const [receivingPc, setReceivingPc] = useState<null | RTCPeerConnection>(
+        null
+    );
+    const [remoteVideoTrack, setRemoteVideoTrack] =
+        useState<MediaStreamTrack | null>(null);
+    const [remoteAudioTrack, setRemoteAudioTrack] =
+        useState<MediaStreamTrack | null>(null);
+    const [remoteMediaStream, setRemoteMediaStream] =
+        useState<MediaStream | null>(null);
+    const remoteVideoRef = useRef<HTMLVideoElement>();
+    const localVideoRef = createRef<HTMLVideoElement>();
 
-    useEffect(() => {}, [name]);
+    useEffect(() => {
+        const socket = io(URL);
+        socket.on("send-offer", async ({ roomId }) => {
+            console.log("sending offer");
+            setLobby(false);
+            const pc = new RTCPeerConnection();
+            setSendingPc(pc);
+            if (localVideoTrack) {
+                pc.addTrack(localVideoTrack);
+            }
+            if (localAudioTrack) {
+                pc.addTrack(localAudioTrack);
+            }
 
-    return <div>Hi {name}</div>;
+            pc.onicecandidate = async (e) => {
+                console.log("receiving ice candidate locally");
+                if (e.candidate) {
+                    socket.emit("add-ice-candidate", {
+                        candidate: e.candidate,
+                        type: "sender",
+                    });
+                }
+            };
+
+            pc.onnegotiationneeded = async () => {
+                console.log("on negotiation neeeded, sending offer");
+                const sdp = await pc.createOffer();
+                //@ts-ignore
+                pc.setLocalDescription(sdp);
+                socket.emit("offer", {
+                    sdp,
+                    roomId,
+                });
+            };
+        });
+
+        socket.on("offer", async ({ roomId, sdp: remoteSdp }) => {
+            console.log("received offer");
+            setLobby(false);
+            const pc = new RTCPeerConnection();
+            pc.setRemoteDescription(remoteSdp);
+            const sdp = await pc.createAnswer();
+            //@ts-ignore
+            pc.setLocalDescription(sdp);
+            const stream = new MediaStream();
+            if (remoteVideoRef.current) {
+                remoteVideoRef.current.srcObject = stream;
+            }
+            setRemoteMediaStream(stream);
+            // trickle ice
+            setReceivingPc(pc);
+
+            pc.onicecandidate = async (e) => {
+                console.log("omn ice candidate on receiving seide");
+                if (e.candidate) {
+                    socket.emit("add-ice-candidate", {
+                        candidate: e.candidate,
+                        type: "receiver",
+                    });
+                }
+            };
+
+            pc.ontrack = ({ track, type }) => {
+                if (type == "audio") {
+                    // setRemoteAudioTrack(track);
+                    // @ts-ignore
+                    remoteVideoRef.current.srcObject.addTrack(track);
+                } else {
+                    // setRemoteVideoTrack(track);
+                    // @ts-ignore
+                    remoteVideoRef.current.srcObject.addTrack(track);
+                }
+                //@ts-ignore
+                remoteVideoRef.current.play();
+            };
+            socket.emit("answer", {
+                roomId,
+                sdp: sdp,
+            });
+        });
+
+        socket.on("answer", ({ roomId, sdp: remoteSdp }) => {
+            setLobby(false);
+            setSendingPc((pc) => {
+                pc?.setRemoteDescription(remoteSdp);
+                return pc;
+            });
+            console.log("loop closed");
+        });
+
+        socket.on("lobby", () => {
+            setLobby(true);
+        });
+
+        socket.on("add-ice-candidate", ({ candidate, type }) => {
+            console.log("add ice candidate from remote");
+            console.log({ candidate, type });
+            if (type == "sender") {
+                setReceivingPc((pc) => {
+                    pc?.addIceCandidate(candidate);
+                    return pc;
+                });
+            } else {
+                setReceivingPc((pc) => {
+                    pc?.addIceCandidate(candidate);
+                    return pc;
+                });
+            }
+        });
+
+        setSocket(socket);
+    }, [name]);
+
+    useEffect(() => {
+        if (localVideoRef.current) {
+            if (localVideoTrack) {
+                localVideoRef.current.srcObject = new MediaStream([
+                    localVideoTrack,
+                ]);
+                localVideoRef.current.play();
+            }
+        }
+    }, [localVideoRef]);
+
+    return (
+        <div>
+            Hi {name}
+            <video autoPlay width={400} height={400} ref={localVideoRef} />
+            {lobby ? "Waiting to connect you to someone" : null}
+            <video
+                autoPlay
+                width={400}
+                height={400}
+                ref={remoteVideoRef as LegacyRef<HTMLVideoElement>}
+            />
+        </div>
+    );
 };
